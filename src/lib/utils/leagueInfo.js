917cae564cece0334e14f9fdf642ed03d23e2a9a
JasonG7234 <JasonG7234@gmail.com>
2023-07-24 20:40:56 -0400

    jpg to png

--- src/lib/utils/leagueInfo.js
+++ src/lib/utils/leagueInfo.js
@@ -126,7 +126,7 @@ export const managers = [
       "rival": {
         name: "Kyle", // Can be anything (usually your rival's name)
         link: 11, // manager array number within this array, or null to link back to all managers page
-        image: "/managers/kyle.jpg", // either a specific manager photo or '/managers/everyone.png' or '/managers/question.png'
+        image: "/managers/kyle.png", // either a specific manager photo or '/managers/everyone.png' or '/managers/question.png'
       },
       "favoritePlayer": 6820, // (optional) this corresponds to the Sleeper player ID (https://api.sleeper.app/v1/players/nfl)
       "valuePosition": "RB", // (optional) Favorite position (QB, WR, RB, TE, etc.)
@@ -215,7 +215,7 @@ export const managers = [
       "name": "Kyle",
       "location": "With Steve", // (optional)
       "bio": "Kyle Joseph Korman, once a fantasy football expert, is slowly reacclimating to the rigors of the sport in between pickleball games at Mannino Park.",
-      "photo": "/managers/kyle.jpg", // square ratio recommended (no larger than 500x500)
+      "photo": "/managers/kyle.png", // square ratio recommended (no larger than 500x500)
       "favoriteTeam": "nyg", // (optional) favorite NFL team, (follows convention: nyj, sea, mia, etc.) MUST BE LOWERCASE
       "mode": "Win Now", // (optional) 'Win Now', 'Dynasty', or 'Rebuild' (anything else and you will need to add a new png to /static/ similar to the 'Rebuild.png' and 'Win Now.png' currently in there)
       "rival": {
